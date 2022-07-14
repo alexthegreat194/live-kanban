f4eed447b9675ebb4ec8076fdf4d39d74adc162c
alexthegreat194 <alexander.gentry@aol.com>
2022-07-14 02:22:15 -0700

    fully functioning

--- index.js
+++ index.js
@@ -2,22 +2,147 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { PrismaClient, prisma } = require("@prisma/client");
 
 require('dotenv').config();
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
+const client = new PrismaClient();
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
+
+io.on('connection', async (socket) => {
+    console.log('a user connected: ', socket.id);
+    socket.emit('dev', 'Welcome to the chat');
+
+    const boards = await client.board.findMany({
+        include: {
+            cards: true
+        }
+    });
+    socket.emit('boards', boards)
+    console.log('sent', 'boards', 'to', socket.id);
+
+    socket.on('new-board', async (data) => {
+        console.log('new board: ', data);
+        
+        const board = await client.board.create({
+            data: {
+                name: data.name,
+                description: data.description
+            }
+        });
+
+        const boards = await client.board.findMany({
+            include: {
+                cards: true
+            }
+        });
+
+        io.emit('boards', boards);
+        console.log('boards refreshed...');
+    });
+
+    socket.on('add-card', async (data) => {
+        console.log('new card: ', data);
+        const card = await client.card.create({
+            data: {
+                name: data.name,
+                status: data.status,
+                boardId: data.boardId
+            }
+        });
+
+        const boards = await client.board.findMany({
+            include: {
+                cards: true
+            }
+        });
+
+        io.emit('boards', boards);
+        console.log('boards refreshed...');
+    });
+
+    socket.on('edit-card', async (data) => {
+        console.log('card edit: ', data);
+        const card = await client.card.update({
+            where: {
+                id: data.cardId
+            },
+            data: {
+                status: data.status
+            }
+        });
+
+        const boards = await client.board.findMany({
+            include: {
+                cards: true
+            }
+        });
+
+        io.emit('boards', boards);
+        console.log('boards refreshed...');
+    });
+
+    socket.on('delete-card', async (data) => { 
+        console.log('card delete: ', data);
+        const card = await client.card.delete({
+            where: {
+                id: data.cardId
+            }
+        });
+
+        const boards = await client.board.findMany({
+            include: {
+                cards: true
+            }
+        });
+
+        io.emit('boards', boards);
+        console.log('boards refreshed...');
+    });
+
+    socket.on('delete-board', async (data) => {
+        console.log('board delete: ', data);
+        const board = await client.board.delete({
+            where: {
+                id: data.boardId
+            }
+        });
+
+        const boards = await client.board.findMany({
+            include: {
+                cards: true
+            }
+        });
+
+        io.emit('boards', boards);
+        console.log('boards refreshed...');
+    });
+});
+
+io.on('message', (socket) => {
+    console.log('message: ', socket.id);
+});
+
+io.on('disconnect', () => {
+    console.log('user disconnected');
+});
+
+io.on('new-board', (socket) => {
+    console.log('new board: ', socket.id);
+    console.table(socket.data);
+});
 
 app.get('/', (req, res) => {
     return res.render('home');
 });
 
 const port = process.env.port;
-app.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`express app listening on port ${port}...`);
 })
---
+++ prisma/migrations/20220713004443_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
